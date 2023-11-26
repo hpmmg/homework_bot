21693a8aaef13d86cab2d57c0bc819e5d689c1c9
Daniil Annenkov <annenkovdaniil.l@gmail.com>
2023-11-26 05:45:13 +0300

    fixes and improvements

--- homework.py
+++ homework.py
@@ -1,21 +1,23 @@
-import os
-
 import logging
-
-from sys import stdout
-
+import sys
 import time
-
-import telegram
+from sys import stdout
+from http import HTTPStatus
 
 import requests
+import telegram
 
-import dotenv
+from exceptions import (
+    EnvVariablesNotAvailable,
+    UnavailableApi,
+    UnknownHomeworkStatus,
+    WrongAnswerFormat
+)
 
-from exceptions import (EnvVariablesNotAvailable, UnavailableApi, 
-                        UnknownHomeworkStatus, WrongAnswerFormat)
-
-dotenv.load_dotenv()
+from config import (
+    PRACTICUM_TOKEN, TELEGRAM_TOKEN, TELEGRAM_CHAT_ID, RETRY_TIME, ENDPOINT,
+    HEADERS, HOMEWORK_STATUSES
+)
 
 
 logger = logging.getLogger(__name__)
@@ -28,56 +30,39 @@ formatter = logging.Formatter(
 handler.setFormatter(formatter)
 
 
-PRACTICUM_TOKEN = os.getenv('PRACTICUM_TOKEN')
-TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
-TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
-
-RETRY_TIME = 600
-ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
-HEADERS = {'Authorization': f'OAuth {PRACTICUM_TOKEN}'}
-
-
-HOMEWORK_STATUSES = {
-    'approved': 'Работа проверена: ревьюеру всё понравилось. Ура!',
-    'reviewing': 'Работа взята на проверку ревьюером.',
-    'rejected': 'Работа проверена: у ревьюера есть замечания.'
-}
-
-
 def send_message(bot, message):
     """Функция для отправки сообщения ботом."""
     try:
-        sent_message = bot.send_message(TELEGRAM_CHAT_ID, message)
-        if sent_message['text'] == message:
-            logger.info(f'Бот отправил сообщение "{message}"')
-        else:
-            logger.error('Ошибка при отправке сообщения в телеграм!')
-    except telegram.error.TelegramError:
-        logger.error('Ошибка при отправке сообщения в телеграм!')
+        bot.send_message(TELEGRAM_CHAT_ID, message)
+        logger.info(f'Бот отправил сообщение "{message}"')
+    except telegram.error.TelegramError as error:
+        logger.error(f'Ошибка {error} при отправке сообщения в телеграм!')
 
 
 def get_api_answer(current_timestamp):
     """Функция для запроса ресурса с API Практикума."""
-    timestamp = current_timestamp or int(time.time())
-    params = {'from_date': timestamp}
-
-    response = requests.get(
-        url=ENDPOINT,
-        headers=HEADERS,
-        params=params
-    )
-    if response.status_code == requests.codes.ok:
-        logger.info(f'Получен ответ от API {response.json()}')
-        return response.json()
-    else:
+    try:
+        timestamp = current_timestamp or int(time.time())
+        params = {'from_date': timestamp}
+        response = requests.get(
+            url=ENDPOINT,
+            headers=HEADERS,
+            params=params
+        )
+        if response.status_code == HTTPStatus.OK.value:
+            logger.info(f'Получен ответ от API {response.json()}')
+            return response.json()
+        else:
+            logger.error('Сбой при запросе к эндпоинту!')
+            raise UnavailableApi('Сбой при запросе к API!')
+    except Exception as error:
         logger.error('Сбой при запросе к эндпоинту!')
-        raise UnavailableApi('Сбой при запросе к API')
+        raise UnavailableApi(f'Сбой при запросе к API! {error}')
 
 
 def check_response(response):
     """Функция для проверки формата ответа от API Практикума."""
-    if (('homeworks' and 'current_date') in response
-       and isinstance(response['homeworks'], list)):
+    if isinstance(response, dict) and isinstance(response['homeworks'], list):
         logger.info('Формат ответа соответствует ожидаемому')
         return response['homeworks']
     logger.error('Формат ответа НЕ соответствует ожидаемому')
@@ -91,8 +76,8 @@ def parse_status(homework):
         homework_status = homework['status']
         verdict = HOMEWORK_STATUSES[homework_status]
         return f'Изменился статус проверки работы "{homework_name}". {verdict}'
-    except KeyError:
-        logger.error('Неожиданный статус работы')
+    except KeyError as error:
+        logger.error(f'Неожиданный статус работы! {error}')
         raise UnknownHomeworkStatus
 
 
@@ -126,8 +111,7 @@ def main():
                 current_timestamp = response['current_date']
             else:
                 logger.critical('Недоступны переменные окружения!')
-                raise EnvVariablesNotAvailable('EnvVarsNotAvail')
-            time.sleep(RETRY_TIME)
+                sys.exit('Недоступны переменные окружения!')
 
         except Exception as error:
             message = f'Сбой в работе программы: {error}'
@@ -139,9 +123,9 @@ def main():
                         api_error_count += 1
                 else:
                     send_message(bot, message)
+
+        finally:
             time.sleep(RETRY_TIME)
-        else:
-            ...
 
 
 if __name__ == '__main__':
